b3529a3177ed0a9c0b4b4338747116fdb405a299
Dima Myronenko <dimyr7@Skynet.local>
2017-02-19 19:18:23 -0600

    fixing PCoA

--- hw3/part-b.py
+++ hw3/part-b.py
@@ -1,7 +1,10 @@
+#! env python
 import numpy as np
 import matplotlib.pyplot as plt
 
 folder_path = "./cifar-10-batches-py/"
+
+subset = 10000
 
 def unpickle(file):
     import cPickle
@@ -18,10 +21,17 @@ def get_split_data(data):
 
 def get_data(batch_file):
     batch = unpickle(folder_path + batch_file)
-    data = np.array(batch["data"], dtype=np.float32)[0:1000]
-    labels = np.array(batch["labels"])[0:1000]
+    data = np.array(batch["data"], dtype=np.float32)[0:subset]
+    labels = np.array(batch["labels"])[0:subset]
     return data, labels
 
+def show_pic(data, name):
+    pic = np.zeros((32,32,3))
+    for i in range(32):
+        for j in range(32):
+            for k in range(3):
+                pic[i,j,k] = data[1024*k + 32*j + i]/256.
+    plt.imsave(fname=name, arr=pic)
 def get_mean(working_set):
     num_dims = len(working_set[0])
     mean = np.zeros(num_dims)
@@ -34,10 +44,8 @@ def get_d(means):
     d = np.zeros((N, N))
     for i in range(N):
         for j in range(N):
-            mean_length = len(means[i])
-            mean_i = means[i].reshape((mean_length,1))
-            mean_j = means[j].reshape((mean_length,1))
-            d[i,j] = np.dot(means[i], means[j])
+            mean_diff = means[i] - means[j]
+            d[i,j] = np.dot(mean_diff, mean_diff)
     return d
 
 meta_info = unpickle(folder_path + "batches.meta")
@@ -49,33 +57,31 @@ num_data = meta_info["num_cases_per_batch"]
 data, labels = get_data("data_batch_1")
 split_data = get_split_data(data)
 
-means = np.zeros((num_data, num_dims))
+means = np.zeros((num_labels, num_dims))
 for label_i in range(num_labels):
     working_set = split_data[label_i]
     means[label_i] = get_mean(working_set)
 
-N = num_data
+N = num_labels
 
-I = np.identity(N)
-ones = np.ones(N)
-ones_T = np.ones(N).reshape((N, 1))
-A = I - 1/float(N) * ones * ones_T
+A = np.identity(N) - 1./N * np.ones((N,N))
 D = get_d(means)
-W = 0.5 * A * D * A.T
+W = -0.5 * A.dot(D).dot(A.T)
 
-cov_mat = np.cov(W)
-eival, eivec = np.linalg.eig(cov_mat)
+eival, eivec = np.linalg.eig(W)
 
 idx = eival.argsort()[::-1]
 eival = eival[idx]
 eivec = eivec[:,idx]
 
-diag = np.array([[eival[0], 0], [0, eigval[1]]])
-diag = diag**0.5
+eival_r = np.diag(eival[0:2])**0.5
+U_r = eivec[:,0:2]
 
-U = eivec[:2]
 
-V_T = diag * U.T
 
-plt.plot(V_T[0], V_T[1])
+
+
+V_T = eival_r.dot(U_r.T)
+print V_T.shape
+plt.plot(V_T[0], V_T[1], 'b.')
 plt.show()
