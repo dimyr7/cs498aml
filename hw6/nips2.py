a0e1992cdd725940f1298129897066c7e4c8389d
bruno calogero <brunocalogero@hotmail.com>
2017-04-04 16:23:36 -0500

    start part2

---
+++ hw6/nips2.py
@@ -0,0 +1,20 @@
+#! env python
+import numpy as np
+import sklearn.cluster
+import random as rand
+import matplotlib as mpl
+mpl.use('Agg')
+import matplotlib.pyplot as plt
+import glob
+from scipy import misc
+
+
+fish = misc.imread("part2images/fish.png")
+flower = misc.imread("part2images/flower.png")
+smallsunset = misc.imread("part2images/smallsunset.png")
+ 
+
+
+
+
+
