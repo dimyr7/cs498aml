f768ef51d1b017412c57dab8d84b47cb5f994c1f
Dima Myronenko <dimyr7@Skynet.local>
2017-04-15 15:01:54 -0500

    unsuccessful noise removal

--- hw7/q1.py
+++ hw7/q1.py
@@ -16,7 +16,6 @@ def display(data):
             else:
                 print ".",
         print ""
-def calc_num(pi_old, noisy_images
 
 
 mndata = MNIST('./data')
@@ -33,22 +32,65 @@ noisy_images = noisy_images.reshape((-1, width, width))
 
 pi_old = np.ones((width,width))/2.
 
+def get_numerator(pi_old, noisy_image, (x,y)):
+    num_exp_sum = 0.
+    if(x > 0.):
+        num_exp_sum += (theta * (2.*pi_old[x-1,y]-1.) + theta * noisy_image[x,y])
+    if(x < width-1):
+        num_exp_sum += (theta * (2.*pi_old[x+1,y]-1.) + theta * noisy_image[x,y])
+    if(y > 0.):
+        num_exp_sum += (theta * (2.*pi_old[x,y-1]-1.) + theta * noisy_image[x,y])
+    if(y < width-1):
+        num_exp_sum += (theta * (2.*pi_old[x,y+1]-1.) + theta * noisy_image[x,y])
+    return np.exp(num_exp_sum)
+def get_denomenator(pi_old, noisy_image, (x,y)):
+    den_exp_sum_left = 0.
+    if(x > 0.):
+        den_exp_sum_left += (theta * (2.*pi_old[x-1,y]-1.) + theta * noisy_image[x,y])
+    if(x < width-1):
+        den_exp_sum_left += (theta * (2.*pi_old[x+1,y]-1.) + theta * noisy_image[x,y])
+    if(y > 0.):
+        den_exp_sum_left += (theta * (2.*pi_old[x,y-1]-1.) + theta * noisy_image[x,y])
+    if(y < width-1):
+        den_exp_sum_left += (theta * (2.*pi_old[x,y+1]-1.) + theta * noisy_image[x,y])
+
+    den_exp_sum_right = 0.
+    if(x > 0.):
+        den_exp_sum_right += -1. * (theta * (2.*pi_old[x-1,y]-1.) + theta * noisy_image[x,y])
+    if(x < width-1):
+        den_exp_sum_right += -1. * (theta * (2.*pi_old[x+1,y]-1.) + theta * noisy_image[x,y])
+    if(y > 0.):
+        den_exp_sum_right += -1. * (theta * (2.*pi_old[x,y-1]-1.) + theta * noisy_image[x,y])
+    if(y < width-1):
+        den_exp_sum_right += -1. * (theta * (2.*pi_old[x,y+1]-1.) + theta * noisy_image[x,y])
+    return np.exp(den_exp_sum_left) + np.exp(den_exp_sum_right)
+
+
+
 def get_new_pi(old_pi, image):
+    new_pi = np.zeros((width, width))
     for x in range(width):
         for y in range(width):
-            num_sum = 0.
-            if(x > 0.):
-                num_sum += (theta * (2.*pi_old[x-1,y]-1.) + theta * noisy_images[x,y])
-            if(x < width-1):
-                num_sum += (theta * (2.*pi_old[x+1,y]-1.) + theta * noisy_images[x,y])
-            if(y > 0.):
-                num_sum += (theta * (2.*pi_old[x,y-1]-1.) + theta * noisy_images[x,y])
-            if(y < width-1):
-                num_sum += (theta * (2.*pi_old[x,y+1]-1.) + theta * noisy_images[x,y])
+            numerator = get_numerator(pi_old, image, (x,y))
+            denominator = get_denomenator(pi_old, image, (x,y))
+            new_pi[x,y]= numerator/denominator
+    return new_pi
 
-def denoise_image(noisy_image):
-    for iteration in range(10):
+
+def denoise_image(image):
+    old_pi = np.ones((width, width))/5.
+    for iteration in range(100):
         new_pi = get_new_pi(old_pi, image)
+        old_pi = new_pi
+    print new_pi[0]
+    new_image = (new_pi > 0.5)+0.
+    return new_image
+
+new_image = denoise_image(noisy_images[0])
+display(noisy_images[0])
+print "======"
+display(new_image)
+exit(1)
 
 for image_idx in range(noisy_images.shape[0]):
     denoise_image(noisy_images[image_idx])
